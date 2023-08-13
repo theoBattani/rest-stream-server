f8eb625e6f1e278db65e4501273bfa1ad6da5031
theo.battani <theobattani@gmail.com>
2023-08-13 16:03:50 +0200

    Initial commit

---
+++ serve-test/src/test/java/fr/battani/test/TestApplicationTests.java
@@ -0,0 +1,12 @@
+package fr.battani.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
